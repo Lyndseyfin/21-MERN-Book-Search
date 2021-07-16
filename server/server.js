4ad34da1e336afd2b11d55ef951898f62063b25f
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-07-16 15:55:53 -0600

    app.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
 
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer} = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 
