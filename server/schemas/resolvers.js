5ad06bf4567bebf0adf238920be8b1798ef29787
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-07-18 12:09:02 -0600

    typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -20,9 +20,7 @@ const resolvers = {
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
-      const token = signToken(user);
-
-      return { token, user };
+      return { token, user};
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
